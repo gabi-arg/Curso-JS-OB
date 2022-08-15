19c43f08d48e18de741d0be48062c41e4fa1cf11
GAbriela Duran <gabrieladuran013@gmail.com>
2022-08-15 17:59:18 -0300

    Ejercicio de tipos de datos y objetos

---
+++ mi-primer-proyecto-de-node/ejecicio2.js
@@ -0,0 +1,16 @@
+const DatosPersonales = [
+    "Gabriela",
+     35,
+     isdeveloper = true,
+     fechaNacimiento= new Date ("June 28 1987"),
+     libro = [
+         Titulo="desing Pateerns",
+         Autor="Erich Gamma",
+         fecha = new Date ("october 1994")
+      ]
+    
+ ];
+ 
+ 
+
+
