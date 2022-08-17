3b8fd6c2b4d13ea209df1b1d619895b47dc763f7
GAbriela Duran <gabrieladuran013@gmail.com>
2022-08-16 23:30:20 -0300

    estructuras de control

---
+++ mi-primer-proyecto-de-node/estructurasDeControl.js
@@ -0,0 +1,30 @@
+/* Crea los siguientes archivos JS:
+
+- factorial-for.js -> Este archivo debe calcular el factorial de 10 utilizando un solo bucle for */
+for (let i = 0; i<10; i++) {
+    console.log (i)
+}
+/* - factorial-while.js -> Este archivo debe calcular el factorial de 10 utilizando un bucle while */
+
+let a = 0;
+let mayor = 10;
+while (a < mayor){
+    console.log (a);
+    a++;
+}
+
+/* - factorial-break.js -> Este archivo debe calcular el factorial de 10 utilizando un bucle while, una bifurcación if y una sentencia break */
+
+let i = 0;
+let max = 10;
+while (i<max){
+    console.log (i);
+    i++;
+    if (i>max){    
+    break
+    }
+}
+ 
+
+
+
