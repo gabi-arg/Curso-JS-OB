4a7f91c73aab7b8aa8ab60b4a64606d17b37b724
GAbriela Duran <gabrieladuran013@gmail.com>
2022-08-17 23:49:51 -0300

    cadena de texto

---
+++ mi-primer-proyecto-de-node/cadenaDeTexto.js
@@ -0,0 +1,20 @@
+/* Crea un archivo JS que contenga las siguientes líneas 
+ - Una cadena de texto con tu Nombre */
+let nombre = 'Gabriela';
+/* - Otra cadena de texto con tu Apellido */
+let apellido = 'Duran';
+/* - Una cadena de texto que se llame "estudiante" concatenando tu Nombre y tu Apellido con un espacio entre medias */
+let estudiante = nombre + "" + apellido;
+/* - Una cadena de texto que se llame "estudianteMayus" que contenga la cadena estudiante pero todo en mayúsculas */
+let estudianteMayus = estudiante.toUpperCase;
+/* - Una cadena de texto que se llame "estudianteMinus" que contenga la cadena estudiante pero todo en minúsculas */
+let estudianteMinus = estudiante.toLowerCase;
+/* - Una variable que contenga el número de letras de la cadena "estudiante" contando los espacios */
+
+/* - Una variable que contenga la primera letra del Nombre */
+/* 
+- Otra variable que contenga la última letra del Apellido */
+
+/* - Una cadena de texto que elimine los espacios de la variable "estudiante" */
+/* 
+- Una variable booleana que diga si el Nombre está contenido en la variable "estudiante" */
