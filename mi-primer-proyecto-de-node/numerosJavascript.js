957be91852bcc95cb69a744c864ef95c3fd4b314
GAbriela Duran <gabrieladuran013@gmail.com>
2022-08-18 20:49:04 -0300

    numeros

---
+++ mi-primer-proyecto-de-node/numerosJavascript.js
@@ -0,0 +1,21 @@
+//Crea un archivo JS que contenga las siguientes líneas
+
+//- Una variable que contenga tu altura en centímetros (entero)
+    let altura = 175;
+
+//- Una variable que contenga tu altura en metros (número de coma flotante)
+
+    let alturaenDecimal = 1.75;
+
+//- Una variable que contenga tu peso en kilogramos (número de coma flotante)
+
+    let peso = 75.5
+
+//- Una variable que contenga tu altura en metros redondeada hacia arriba
+
+    let alturaRedondeada = alturaenDecimal.toFixed();
+
+//- Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+    let pesoRedondeado = peso.Mathfloor();
+//- Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+    let max_valor = Number.MAX_VALUE;
