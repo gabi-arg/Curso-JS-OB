b2f733e709690a2f4ee54300c4e87734e6da4ae5
GAbriela Duran <gabrieladuran013@gmail.com>
2022-08-26 23:39:50 -0300

    Ejercicio consola

---
+++ mi-primer-proyecto-de-node/consola.js
@@ -0,0 +1,15 @@
+//- Abre una nueva ventana "about:blank" en Google Chrome
+
+//- Abre la consola de desarrollador de Google Chrome (F12)
+
+//- Pregunta al usuario cuál es su edad y almacénala en una variable llamada edad
+
+
+//Escribo por consola
+const edad = prompt('¿Cual es tu edad?')
+undefined
+
+//Mando a llamar la variable
+edad
+//Consola responde
+'35'
