5883c664788f321fb7283338c76ef4a96fb894ab
GAbriela Duran <gabrieladuran013@gmail.com>
2022-10-28 17:56:13 -0300

    Sesion 12

---
+++ sesios-12-Intro-Depuracion/index.js
@@ -0,0 +1,13 @@
+const persona = {
+    nombre: 'Gabi',
+    edad: 34
+}
+
+function obtenDobleEdad(edad){
+    return 2 * edad
+}
+
+const dobleEdad = obtenDobleEdad(persona.edad)
+
+console.log(dobleEdad)
+console.log(persona)
